8b5e3003bcf12ed5e090103e464af0c751497791
lreshmaprdxn <reshmal@prdxn.com>
2020-03-17 17:58:22 +0530

    function for video modal added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,53 @@
+// Function for Navigation sidebar
 var hamburger = document.querySelector ('.hamburger');
 var nav = document.querySelector ('.menu');
-hamburger.addEventListener ('click',navShow);
+hamburger.addEventListener ('click',showNav);
 
-// Function for Navigation sidebar
-function navShow() {
+function showNav() {
   nav.classList.toggle('show');  
   hamburger.classList.toggle('open');
   document.querySelector('html').classList.toggle('no-scroll');
 }
+
+// Function for video modal
+var modal = document.querySelector('.lightbox-opened');
+var videoThumbnail = document.querySelectorAll('.video figure');
+var thumbnailArray = Array.from(videoThumbnail);
+
+thumbnailArray.forEach(function(element) {
+	element.addEventListener('click',openModal);
+});
+
+modal.addEventListener('click',closeModal);
+document.querySelector('body').addEventListener('keyup',closeOnEsc);
+
+// Function to open modal
+function openModal(e) {
+	e.preventDefault();
+	var currentVideo = this.nextElementSibling.src;
+	modal.children[0].src = currentVideo ;
+	modal.classList.add("activeVideo");
+	document.querySelector('html').classList.add('no-scroll');
+};
+
+// Function to close modal
+function closeModal(e) {
+	e.preventDefault();
+	var playedVideo = document.querySelector('.lightbox-opened .currentVideo');
+	if(e.target != playedVideo) {
+		modal.classList.remove("activeVideo");
+		document.querySelector('html').classList.remove('no-scroll');
+	}
+};
+
+// Function to close modal on esc button
+function closeOnEsc(e) {
+	e.preventDefault();
+	if(e.which == 27) {
+		modal.classList.remove("activeVideo");
+		document.querySelector('html').classList.remove('no-scroll');
+	}
+};
 
 // Function for banner slider
 $(document).ready (function() {
@@ -17,24 +57,3 @@ $(document).ready (function() {
   arrows: false,
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
