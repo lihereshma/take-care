e1a973b18a5983ad333dc2ca70d909f80e7b5dbd
lreshmaprdxn <reshmal@prdxn.com>
2020-03-17 17:18:48 +0530

    function for banner slider added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,21 @@ var hamburger = document.querySelector ('.hamburger');
 var nav = document.querySelector ('.menu');
 hamburger.addEventListener ('click',navShow);
 
-// Navigation sidebar function
+// Function for Navigation sidebar
 function navShow() {
   nav.classList.toggle('show');  
   hamburger.classList.toggle('open');
   document.querySelector('html').classList.toggle('no-scroll');
 }
 
-
+// Function for banner slider
+$(document).ready (function() {
+  $('.slider').slick ({
+  slidesToShow: 1,
+  dots: true,
+  arrows: false,
+  });
+});
 
 
 
