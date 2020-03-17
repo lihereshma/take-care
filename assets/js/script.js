b7d7b3d6fd6f3036470af5005ed0b76a3560f070
lreshmaprdxn <reshmal@prdxn.com>
2020-03-17 17:11:44 +0530

    navigation sidebar function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-/* Author: 
+var hamburger = document.querySelector ('.hamburger');
+var nav = document.querySelector ('.menu');
+hamburger.addEventListener ('click',navShow);
 
-*/
+// Navigation sidebar function
+function navShow() {
+  nav.classList.toggle('show');  
+  hamburger.classList.toggle('open');
+  document.querySelector('html').classList.toggle('no-scroll');
+}
 
 
 
