0e34aca1db27c8d1ed8a051f664f04b5dd0daa53
lreshmaprdxn <reshmal@prdxn.com>
2020-03-19 15:12:26 +0530

    js end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,64 @@
-// Function for navigation sidebar
-var hamburger = document.querySelector ('.hamburger');
-var nav = document.querySelector ('.menu');
-hamburger.addEventListener ('click',showNav);
+window.onload = function() {
+	// Function for navigation sidebar
+	var hamburger = document.querySelector('.hamburger');
+	var nav = document.querySelector('.menu');
+	hamburger.addEventListener('click',showNav);
 
-function showNav() {
-  nav.classList.toggle('show');  
-  hamburger.classList.toggle('open');
-  document.querySelector('html').classList.toggle('no-scroll');
+	function showNav() {
+		nav.classList.toggle('show');  
+		hamburger.classList.toggle('open');
+		document.querySelector('html').classList.toggle('no-scroll');
+	}
+
+	// Function for video modal
+	var modal = document.querySelector('.lightbox-opened');
+	var videoThumbnail = document.querySelectorAll('.video figure');
+	var thumbnailArray = Array.from(videoThumbnail);
+
+	thumbnailArray.forEach(function(element) {
+		element.addEventListener('click',openModal);
+	});
+
+	modal.addEventListener('click',closeModal);
+	document.querySelector('body').addEventListener('keyup',closeOnEsc);
+
+	// Function to open modal
+	function openModal(e) {
+		e.preventDefault();
+		var currentVideo = this.nextElementSibling.src;
+		modal.children[0].src = currentVideo ;
+		modal.classList.add("activeVideo");
+		document.querySelector('html').classList.add('no-scroll');
+	};
+
+	// Function to close modal
+	function closeModal(e) {
+		e.preventDefault();
+		var playedVideo = document.querySelector('.lightbox-opened .currentVideo');
+		if(e.target != playedVideo) {
+			modal.classList.remove("activeVideo");
+			document.querySelector('html').classList.remove('no-scroll');
+		}
+	};
+
+	// Function to close modal on esc button
+	function closeOnEsc(e) {
+		e.preventDefault();
+		if(e.which === 27) {
+			modal.classList.remove("activeVideo");
+			document.querySelector('html').classList.remove('no-scroll');
+		}
+	};
 }
 
-// Function for video modal
-var modal = document.querySelector('.lightbox-opened');
-var videoThumbnail = document.querySelectorAll('.video figure');
-var thumbnailArray = Array.from(videoThumbnail);
-
-thumbnailArray.forEach(function(element) {
-	element.addEventListener('click',openModal);
-});
-
-modal.addEventListener('click',closeModal);
-document.querySelector('body').addEventListener('keyup',closeOnEsc);
-
-// Function to open modal
-function openModal(e) {
-	e.preventDefault();
-	var currentVideo = this.nextElementSibling.src;
-	modal.children[0].src = currentVideo ;
-	modal.classList.add("activeVideo");
-	document.querySelector('html').classList.add('no-scroll');
-};
-
-// Function to close modal
-function closeModal(e) {
-	e.preventDefault();
-	var playedVideo = document.querySelector('.lightbox-opened .currentVideo');
-	if(e.target != playedVideo) {
-		modal.classList.remove("activeVideo");
-		document.querySelector('html').classList.remove('no-scroll');
-	}
-};
-
-// Function to close modal on esc button
-function closeOnEsc(e) {
-	e.preventDefault();
-	if(e.which == 27) {
-		modal.classList.remove("activeVideo");
-		document.querySelector('html').classList.remove('no-scroll');
-	}
-};
-
 // Function for banner slider
-$(document).ready (function() {
-  $('.slider').slick ({
-  slidesToShow: 1,
-  dots: true,
-  arrows: false,
+$(document).ready(function() {
+  $('.banner-slider').slick ({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,	
+		dots: true,
+		arrows: false,		
   });
 });
